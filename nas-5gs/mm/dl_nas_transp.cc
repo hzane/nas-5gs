9037e1ff9d10eac660337e706189927bb4bebf23
hunks <hearts.zhang@gmail.com>
2019-09-03 21:54:53 +0800

    -

--- nas-5gs/mm/dl_nas_transp.cc
+++ nas-5gs/mm/dl_nas_transp.cc
@@ -6,12 +6,13 @@ namespace mm_dl_nas_transp {
 extern const element_meta pld_cont_type;
 extern const element_meta pld_cont;
 extern const element_meta pdu_ses_id;
-extern const element_meta add_inf;
+extern const element_meta additional_inf;
 extern const element_meta backoff_gprs_timer3;
 } // namespace mm_dl_nas_transp
 
 using namespace nas;
 using namespace mm;
+using namespace mm_dl_nas_transp;
 
 /*
  * 8.2.11 DL NAS transport
@@ -20,49 +21,41 @@ int mm::dl_nas_transp(dissector d, context* ctx) {
     auto        len = d.length;
     use_context uc(ctx, "dl-nas-transport");
 
-    using namespace mm_dl_nas_transp;
 
     /* Initalize the private struct */
     // nas5gs_get_private_data(pinfo);
 
     /*Payload container type    Payload container type     9.11.3.40    M    V    1/2 H0*/
-    /*Spare half octet    Spare half octet    9.5    M    V    1/2 H1*/
-    d.tree->add_item(d.pinfo, d.tvb, d.offset, 1, hf_spare_half_octet, enc::be);
-
     // ELEM_MAND_V(DE_NAS_5GS_MM_PLD_CONT_TYPE,ei_nas_5gs_missing_mandatory_elemen);
     auto consumed = dissect_elem_v(nullptr, &pld_cont_type, d, ctx);
-    d.offset += consumed;
-    d.length -= consumed;
+    /*Spare half octet    Spare half octet    9.5    M    V    1/2 H1*/
+    d.tree->add_item(d.pinfo, d.tvb, d.offset, 1, hf_spare_half_octet, enc::be);
+    d.step(1);
 
-    /*Payload container    Payload container    9.11.3.39    M    LV-E    3-65537*/
+    /*Payload container     9.11.3.39    M    LV-E    3-65537*/
     // ELEM_MAND_LV_E(DE_NAS_5GS_MM_PLD_CONT,ei_nas_5gs_missing_mandatory_elemen);
     consumed = dissect_elem_lv_e(nullptr, &pld_cont, d, ctx);
-    d.offset += consumed;
-    d.length -= consumed;
+    d.step(consumed);
 
-    /*12    PDU session ID    PDU session identity 2 9.11.3.37    C    TV    2 */
+    /*12    PDU session ID    PDU session identity 2 9.11.3.41    C    TV    2 */
     // ELEM_OPT_TV( 0x12, , DE_NAS_5GS_MM_PDU_SES_ID_2, " - PDU session ID");
     consumed = dissect_opt_elem_tv(nullptr, &pdu_ses_id, d, ctx);
-    d.offset += consumed;
-    d.length -= consumed;
+    d.step(consumed);
 
-    /*24    Additional information    Additional information    9.10.2.1    O    TLV 3-n*/
+    /*24    Additional information    Additional information    9.11.2.1    O    TLV 3-n*/
     // ELEM_OPT_TLV(0x24, , DE_NAS_5GS_CMN_ADD_INF, NULL);
-    consumed = dissect_opt_elem_tlv(nullptr, &add_inf, d, ctx);
-    d.offset += consumed;
-    d.length -= consumed;
+    consumed = dissect_opt_elem_tlv(nullptr, &additional_inf, d, ctx);
+    d.step(consumed);
 
     /*58    5GMM cause    5GMM cause 9.11.3.2    O    TV    2 */
     // ELEM_OPT_TV(0x58, NAS_5GS_PDU_TYPE_MM, DE_NAS_5GS_MM_5GMM_CAUSE, NULL);
     consumed = dissect_opt_elem_tv(nullptr, &mm_cause, d, ctx);
-    d.offset += consumed;
-    d.length -= consumed;
+    d.step(consumed);
 
-    /*37    Back-off timer value    GPRS timer 3 9.10.2.5    O    TLV    3 */
+    /*37    Back-off timer value    GPRS timer 3 9.11.2.5    O    TLV    3 */
     // ELEM_OPT_TLV(0x37, GSM_A_PDU_TYPE_GM, DE_GPRS_TIMER_3, " - Back-off timer value");
     consumed = dissect_opt_elem_tlv(nullptr, &backoff_gprs_timer3, d, ctx);
-    d.offset += consumed;
-    d.length -= consumed;
+    d.step(consumed);
 
     d.extraneous_data_check(0);
     return len;
@@ -70,7 +63,7 @@ int mm::dl_nas_transp(dissector d, context* ctx) {
 
 namespace mm_dl_nas_transp {
 int dissect_pdu_ses_id(dissector d, context* ctx);
-int dissect_add_inf(dissector d, context* ctx);
+int dissect_additional_inf(dissector d, context* ctx);
 
 extern const element_meta pld_cont_type = {
     0xff,
@@ -90,10 +83,10 @@ extern const element_meta pdu_ses_id = {
     dissect_pdu_ses_id,
 };
 
-extern const element_meta add_inf = {
+extern const element_meta additional_inf = {
     0x24,
     "Additional information",
-    dissect_add_inf,
+    dissect_additional_inf,
 };
 
 
@@ -114,8 +107,8 @@ int dissect_pdu_ses_id(dissector d, context* ctx) {
 }
 
 /* 9.10.2.1    Additional information*/
-int dissect_add_inf(dissector d, context* ctx) {
-    diag("no dissect for %s\n", "additional-information");
+int dissect_additional_inf(dissector d, context* ctx) {
+    diag("no dissect for %s\n", paths(ctx).c_str());
     return d.length;
 }
 
--- nas-5gs/mm/ul_nas_transp.cc
+++ nas-5gs/mm/ul_nas_transp.cc
@@ -5,7 +5,7 @@ namespace mm_ul_nas_transp {
 extern const element_meta pld_cont_type;
 extern const element_meta pld_cont;
 extern const element_meta pdu_ses_id;
-extern const element_meta add_inf;
+extern const element_meta additional_inf;
 extern const element_meta old_pdu_ses_id;
 extern const element_meta req_type;
 extern const element_meta s_nssai;
@@ -39,7 +39,7 @@ int mm::ul_nas_transp(dissector d, context* ctx) {
     consumed = dissect_elem_lv_e(nullptr, &pld_cont, d, ctx);
     d.step(consumed);
 
-    /*12    PDU session ID 2 9.11.3.41    C    TV    2 */
+    /*12 PDU session ID 2 9.11.3.41    C    TV    2 */
     // ELEM_OPT_TV( 0x12, , DE_NAS_5GS_MM_PDU_SES_ID_2, " - PDU session ID");
     consumed = dissect_opt_elem_tv(nullptr, &pdu_ses_id, d, ctx);
     d.step(consumed);
@@ -49,24 +49,24 @@ int mm::ul_nas_transp(dissector d, context* ctx) {
     consumed = dissect_opt_elem_tv(nullptr, &old_pdu_ses_id, d, ctx);
     d.step(consumed);
 
-    /*8-    Request type    Request type    9.11.3.47    O    TV    1 */
+    /*8-    Request type  9.11.3.47    O    TV    1 */
     //    ELEM_OPT_TV_SHORT(0x80, , DE_NAS_5GS_MM_REQ_TYPE, NULL);
     consumed = dissect_opt_elem_tv_short(nullptr, &req_type, d, ctx);
     d.step(consumed);
 
-    /*22    S-NSSAI    S-NSSAI    9.11.2.8    O    TLV    3-10 */
+    /*22    S-NSSAI   9.11.2.8    O    TLV    3-10 */
     //    ELEM_OPT_TLV(0x22, , DE_NAS_5GS_CMN_S_NSSAI, NULL);
     consumed = dissect_opt_elem_tlv(nullptr, &s_nssai, d, ctx);
     d.step(consumed);
 
-    /*25    DNN    DNN    9.11.2.1A    O    TLV    3-102 */
+    /*25    DNN 9.11.2.1A    O    TLV    3-102 */
     //    ELEM_OPT_TLV(0x25, , DE_NAS_5GS_CMN_DNN, NULL);
     consumed = dissect_opt_elem_tlv(nullptr, &dnn, d, ctx);
     d.step(consumed);
 
     /*24 Additional information  9.11.2.1    O    TLV 3-n */
     // ELEM_OPT_TLV(0x24, , DE_NAS_5GS_CMN_ADD_INF, NULL);
-    consumed = dissect_opt_elem_tlv(nullptr, &add_inf, d, ctx);
+    consumed = dissect_opt_elem_tlv(nullptr, &additional_inf, d, ctx);
     d.step(consumed);
 
     // Z	MA PDU session information	MA PDU session information	O	TV	1
@@ -81,7 +81,7 @@ namespace mm_ul_nas_transp {
 int dissect_pld_cont_type(dissector d, context* ctx); // 9.11.3.40
 int dissect_pld_cont(dissector d, context* ctx);      // 9.11.3.39
 int dissect_pdu_ses_id(dissector d, context* ctx);
-int dissect_add_inf(dissector d, context* ctx);
+int dissect_additional_inf(dissector d, context* ctx);
 int dissect_old_pdu_ses_id(dissector d, context* ctx);
 int dissect_req_type(dissector d, context* ctx);
 
@@ -98,32 +98,43 @@ extern const element_meta pld_cont = {
     "Payload container",
     dissect_pld_cont,
 };
+
+// PDU session ID 2 9.11.3.41
 extern const element_meta pdu_ses_id = {
     0x12,
     "PDU session ID",
     dissect_pdu_ses_id,
 };
-extern const element_meta add_inf = {
+
+// Additional information  9.11.2.1
+extern const element_meta additional_inf = {
     0x24,
     "Additional information",
-    dissect_add_inf,
+    dissect_additional_inf,
 };
 
+// PDU session identity 2 9.11.3.41
 extern const element_meta old_pdu_ses_id = {
     0x59,
     "Old PDU session ID",
     dissect_old_pdu_ses_id,
 };
+
+// Request type  9.11.3.47
 extern const element_meta req_type = {
     0x80,
     "Request type    Request type",
     dissect_req_type,
 };
+
+// S-NSSAI   9.11.2.8
 extern const element_meta s_nssai = {
     0x22,
     "S-NSSAI",
     dissect_s_nssai,
 };
+
+// DNN 9.11.2.1A
 extern const element_meta dnn = {
     0x25,
     "DNN",
@@ -138,12 +149,17 @@ int dissect_pld_cont_type(dissector d, context* ctx) {
 // Payload container  9.11.3.39
 int dissect_pld_cont(dissector d, context* ctx) { return mm::dissect_pld_cont(d, ctx); }
 
+// PDU session ID 2 9.11.3.41
 int dissect_pdu_ses_id(dissector d, context* ctx) {
+    // identity value as defined in 3GPP TS 24.007 [11]
     d.add_item(1, hf_pdu_sess_id, enc::be);
     return 1;
 }
-int dissect_add_inf(dissector d, context* ctx) {
-    diag("no dissect for %s\n", "additional-information");
+
+// Additional information  9.11.2.1
+int dissect_additional_inf(dissector d, context* ctx) {
+    // The coding of the additional information value is dependent on the LCS application.
+    diag("no dissect for %s\n", paths(ctx).c_str());
     return d.length;
 }
 
