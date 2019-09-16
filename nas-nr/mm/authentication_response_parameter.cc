36251841328e3140502f22ed2292ebf728c6922d
hunks <hearts.zhang@gmail.com>
2019-09-16 17:52:58 +0800

    Update authentication_response_parameter.cc

--- nas-nr/mm/authentication_response_parameter.cc
+++ nas-nr/mm/authentication_response_parameter.cc
@@ -8,10 +8,9 @@ int mm::dissect_auth_resp_param(dissector d, context* ctx) {
     const use_context uc(ctx, "auth-response-param", d, -1);
     //  9.9.3.4 in 3GPP TS 24.301 [15].
     /* This IE contains either the SRES or the 4 most significant octets of the RES */
-    auto i = d.add_item(4, &hf_dtap_sres, enc::be);
+    (void) d.add_item(4, &hf_dtap_sres, enc::be);
     d.step(4);
 
-    unused(i);
     return 4;
 }
 
