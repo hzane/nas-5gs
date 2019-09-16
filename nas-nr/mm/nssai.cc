c96ee210d664693bac1616fb8ae5f6a0e357d3de
hunks <hearts.zhang@gmail.com>
2019-09-16 17:55:05 +0800

    Update nssai.cc

--- nas-nr/mm/nssai.cc
+++ nas-nr/mm/nssai.cc
@@ -19,16 +19,16 @@ int mm::dissect_configured_nssai(dissector d, context* ctx) {
     /*S-NSSAI value is coded as the length and value part of S-NSSAI information element
      * as specified in subclause 9.11.2.8 starting with the second octet.*/
 
-    auto i = 1;
+    NASNR_AUTO(int) i = 1;
     while (d.length > 0) {
-        const auto subtree = d.add_item(2, "S-NSSAI %u", i++);
+        NASNR_AUTO(proto_node*) const subtree = d.add_item(2, "S-NSSAI %u", i++);
         use_tree   ut(d, subtree);
 
-        const auto length = static_cast< int >(d.tvb->uint8(d.offset));
-        // auto n = d.add_item(1, &hf_mm_length, enc::be);
+        NASNR_AUTO(int) const length = static_cast< int >(d.tvb->uint8(d.offset));
+        // NASNR_AUTO(proto_node*) n = d.add_item(1, &hf_mm_length, enc::be);
         d.step(1);
 
-        const auto consumed = cmn::dissect_s_nssai(d.slice(length), ctx);
+        NASNR_AUTO(int) const consumed = cmn::dissect_s_nssai(d.slice(length), ctx);
         d.step(consumed);
         
     }
@@ -44,16 +44,16 @@ as specified in subclause 9.11.2.8 starting with the second octet.
 int mm::dissect_allowed_nssai(dissector d, context* ctx) {
     const use_context uc(ctx, "allowed-nssai", d, 0);
 
-    auto i = 1;
+    NASNR_AUTO(int) i = 1;
     while (d.length > 0) {
-        const auto subtree = d.add_item(-1, "S-NSSAI %u", i++);
+        NASNR_AUTO(proto_node*) const subtree = d.add_item(-1, "S-NSSAI %u", i++);
         use_tree   ut(d, subtree);
 
-        const auto l    = d.uint8();
-        // auto       item = d.add_item(1, &hf_mm_length, enc::be);
+        NASNR_AUTO(uint8_t) const l    = d.uint8();
+        // NASNR_AUTO(proto_node*)       item = d.add_item(1, &hf_mm_length, enc::be);
         d.step(1);
 
-        const auto consumed = dissect_s_nssai(d.slice(l), ctx);
+        const NASNR_AUTO(int) consumed = dissect_s_nssai(d.slice(l), ctx);
         d.step(consumed);
         d.tree->set_length(consumed + 1);
         
@@ -65,17 +65,17 @@ int mm::dissect_allowed_nssai(dissector d, context* ctx) {
 int mm::dissect_requested_nssai(dissector d, context* ctx) {
     const use_context uc(ctx, "requested-nssai", d, 0);
 
-    auto i = 1;
+    NASNR_AUTO(int) i = 1;
     while (d.length > 0) {
-        auto     subtree = d.add_item(2, "S-NSSAI %u", i++);
+        NASNR_AUTO(proto_node*)     subtree = d.add_item(2, "S-NSSAI %u", i++);
         use_tree ut(d, subtree);
 
-        const auto length = static_cast< int >(d.tvb->uint8(d.offset));
-        // auto n = d.add_item(1, &hf_mm_length, enc::be);
+        NASNR_AUTO(int) const length = static_cast< int >(d.tvb->uint8(d.offset));
+        // NASNR_AUTO(proto_node*) n = d.add_item(1, &hf_mm_length, enc::be);
         d.step(1);
 
         // 9.11.2.8
-        const auto consumed = dissect_s_nssai(d.slice(length), ctx);
+        NASNR_AUTO(int) const consumed = dissect_s_nssai(d.slice(length), ctx);
         d.step(consumed);
         subtree->set_length(length + 1);
         
