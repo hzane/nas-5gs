262b4c930f8251297ceec1d4e1226fe36ae5c4fd
hunks <hearts.zhang@gmail.com>
2019-09-16 17:54:25 +0800

    Update n1_mode_s1_mode_nas_container.cc

--- nas-nr/common/n1_mode_s1_mode_nas_container.cc
+++ nas-nr/common/n1_mode_s1_mode_nas_container.cc
@@ -4,10 +4,8 @@
 // 9.11.2.7 N1 mode to S1 mode NAS transparent container page.350
 int cmn::dissect_n1_to_s1_mode_container(dissector d, context* ctx) {
     const use_context uc(ctx, "n1-mode-to-s1-mode-transparent-container", d, 0);
-    auto i = d.add_item(1, &hf_seq_no, enc::be);
+    (void) d.add_item(1, &hf_seq_no, enc::be);
     d.step(1);
-
-    unused(i);
 
     return uc.length;
 }
