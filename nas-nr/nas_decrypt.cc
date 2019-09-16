d7f21ed0e65258cecac0ab299b988ec63045429a
hunks <hearts.zhang@gmail.com>
2019-09-16 17:48:56 +0800

    Update nas_decrypt.cc

--- nas-nr/nas_decrypt.cc
+++ nas-nr/nas_decrypt.cc
@@ -1,27 +1,5 @@
 #include "config.hh"
 
-int nas_nr_decrypt(const uint8_t*       from,
-                   uint8_t*             to,
-                   int                  length,
-                   uint8_t              type,
-                   uint32_t             code,
-                   uint8_t              seq,
-                   nr_security_context* ctx){
-    diag("in-nas-nr-decrypt\n");
-    if (!is_security_type_cyphered(type)) return 0;
-
-    if (security_ctx_is_nea0(ctx)) return 0;
-
-    if (security_ctx_is_nea1(ctx)) {
-        return nas_nr_decrypt_nea1(from, to, length, seq, ctx);
-    }
-    if (security_ctx_is_nea2(ctx)) {
-        return nas_nr_decrypt_nea2(from, to, length, seq, ctx);
-    }
-    memcpy(to, from, length);
-    return length;
-}
-
 
 bool is_security_type_cyphered(uint8_t type) { return false; }
 
@@ -52,3 +30,26 @@ int nas_nr_decrypt_nea2(const uint8_t*       from,
                         nr_security_context* ctx) {
     return 0;
 }
+
+int nas_nr_decrypt(const uint8_t*       from,
+                   uint8_t*             to,
+                   int                  length,
+                   uint8_t              type,
+                   uint32_t             code,
+                   uint8_t              seq,
+                   nr_security_context* ctx) {
+    diag("in-nas-nr-decrypt\n");
+    if (!is_security_type_cyphered(type)) return 0;
+
+    if (security_ctx_is_nea0(ctx)) return 0;
+
+    if (security_ctx_is_nea1(ctx)) {
+        return nas_nr_decrypt_nea1(from, to, length, seq, ctx);
+    }
+    if (security_ctx_is_nea2(ctx)) {
+        return nas_nr_decrypt_nea2(from, to, length, seq, ctx);
+    }
+    memcpy(to, from, length);
+    return length;
+}
+
