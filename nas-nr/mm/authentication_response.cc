532096f34757fcbfb8544283cfe0997b97f5c178
hunks <hearts.zhang@gmail.com>
2019-09-16 17:53:28 +0800

    Update authentication_response.cc

--- nas-nr/mm/authentication_response.cc
+++ nas-nr/mm/authentication_response.cc
@@ -9,7 +9,7 @@ int mm::authentication_resp(dissector d, context* ctx) {
     const use_context uc(ctx, "authentication-response", d, 0);
 
     /* 2D    Authentication response parameter 9.11.3.17    O    TLV    18 */
-    auto consumed = dissect_opt_tlv(nullptr, &auth_resp_param, d, ctx);
+    NASNR_AUTO(int) consumed = dissect_opt_tlv(nullptr, &auth_resp_param, d, ctx);
     d.step(consumed);
 
     /* 78 EAP message    EAP message     9.11.2.2    O    TLV-E    7-1503 */
