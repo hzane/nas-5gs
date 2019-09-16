35c4fc3cb361ca1b0aa5e57b4052b419d2024e3d
hunks <hearts.zhang@gmail.com>
2019-09-16 17:43:55 +0800

    Update packet_info.hh

--- nas-nr/packet_info.hh
+++ nas-nr/packet_info.hh
@@ -8,18 +8,18 @@ struct context;
 
 
 namespace direction {
-inline extern const int unknown = 0;
-inline extern const int ul      = 1;
-inline extern const int dl      = 2;
-inline extern const int both    = 3;
+NASNR_INLINE const int unknown = 0;
+NASNR_INLINE const int ul      = 1;
+NASNR_INLINE const int dl      = 2;
+NASNR_INLINE const int both    = 3;
 } // namespace direction
 
 struct packet_info {
-    uint32_t presence_flags = 0; // some flags
-    uint32_t num            = 0; // frame number
-    int64_t  abs_ts         = 0; // nano seconds
-    int64_t  rel_ts         = 0; // nano seconds
-    int      dir            = 0; // direction
+    uint32_t presence_flags NASNR_EQUAL_INIT( 0); // some flags
+    uint32_t num            NASNR_EQUAL_INIT( 0); // frame number
+    int64_t  abs_ts         NASNR_EQUAL_INIT( 0); // nano seconds
+    int64_t  rel_ts         NASNR_EQUAL_INIT( 0); // nano seconds
+    int      dir            NASNR_EQUAL_INIT( 0); // direction
 };
 inline void up_link(packet_info*pinfo){
     if (pinfo) pinfo->dir = direction::ul;
