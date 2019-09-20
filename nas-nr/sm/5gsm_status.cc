29cc85de9f5999636f967b65c7230ea7e54150ee
hzane <hearts.zhang@gmail.com>
2019-09-20 09:26:14 +0800

    - FIXED direction

--- nas-nr/sm/5gsm_status.cc
+++ nas-nr/sm/5gsm_status.cc
@@ -2,15 +2,19 @@
 #include "../packet_info.hh"
 
 /* 8.3.16 5GSM status */
-int sm::dissect_sm_status(dissector d, context* ctx) {    
+int sm::dissect_sm_status(dissector d, context* ctx) {
     const use_context uc(ctx, "5gsm-status", d, -1);
 
     /* Direction: both */
-    both_link(d.pinfo);    
+    // both_link(d.pinfo);
 
     /* 5GSM cause    5GSM cause 9.11.4.1    M    V    1 */
     (void) dissect_sm_cause(d, ctx);
     d.step(1);
-    
+
     return 1;
 }
+
+struct sm_status_t {
+    uint8_t cause;
+};
