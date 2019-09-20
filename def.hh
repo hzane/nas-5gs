4bcc46582aca3fe2c025386397d6982d3a855963
hzane <hearts.zhang@gmail.com>
2019-09-20 16:53:37 +0800

    - ADDED some symbols

--- def.hh
+++ def.hh
@@ -178,3 +178,126 @@ struct network_feature_support{
     bool ims_emc;
     bool ims_vops;
 };
+
+namespace sm {
+struct max_data_rate_t {};
+struct uplink_integrity_protection_max_data_rate_t : max_data_rate_t {};
+struct downlink_integrity_protection_max_data_rate_t : max_data_rate_t {};
+
+struct nrsm_cause_t {};
+
+struct ssc_mode_t {};
+struct eps_bearer_content_t {
+    uint8_t operation_code;
+    uint8_t ebit;
+    uint8_t deb;
+};
+struct gprs_timer3_t {};
+struct ssc_mode_t {};
+struct ssc_mode_1_t : ssc_mode_t  {};
+struct ssc_mode_2_t : ssc_mode_t {};
+struct ssc_mode_3_t : ssc_mode_t {};
+
+struct extended_pco_t {};
+struct congestion_reattempt_t {};
+struct nrsm_cause_t {};
+struct eap_message_t {};
+
+struct pdu_session_establishment_reject_t {
+    uint8_t                cause;
+    gprs_timer3_t          backoff_timer;
+    ssc_mode_t             allowed;
+    eap_message_t          eap_message;
+    extended_pco_t         extended_pco;
+    congestion_reattempt_t congestion_reattempt;
+};
+
+} // namespace sm
+
+namespace mm{
+struct imeisv_mobile_id_t {};
+struct pdu_session_status_t {};
+struct rejected_nssai_t {};
+struct tracking_area_id_list_t {};
+struct allowed_nssai_t {};
+struct nssai_t {};
+struct eps_bearer_context_status_t {};
+struct nas_message_container_t {};
+struct nas_ksi_t {};
+struct request_type_t {};
+struct old_pud_session_id_t {};
+struct pdu_session_id_t {};
+struct uplink_data_status_t {};
+struct data_status_t {};
+struct downlink_data_status_t {};
+struct s_tmsi_t {};
+struct pdu_session_reactive_result_error_t {};
+struct pdu_session_reactivation_result_t {};
+struct replayed_s1_ue_security_capability_t {};
+struct additional_security_information_t {};
+struct selected_eps_security_algo_t {};
+struct eps_security_algorithm_t {};
+struct imeisv_request_t {};
+struct imeisv_t {};
+struct replayed_ue_security_capability_t {};
+struct plmn_list_t {};
+struct plmn_t {};
+struct gprs_timer3 {};
+struct backoff_gprs_timer3 {};
+struct pdu_session_id_t {};
+struct mobile_id_t {};
+struct configured_nssai_t {};
+struct timezone_time_t {};
+struct sms_indication_t {};
+struct operator_defined_access_category_t {};
+struct local_timezone_t {};
+struct network_name {};
+struct short_network_name {};
+struct full_network_name {};
+struct daylight_saving_time_t {};
+struct configure_update_indication_t {};
+struct authentication_response_parameter_t {};
+struct authentication_parameter_autn {};
+struct authentication_parameter_rand_t {};
+struct authentication_failure_parameter_t {};
+struct registration_result_t {};
+struct n3gpp_network_provided_policies_t {};
+struct negotiated_drx_parametereter_t {};
+struct operator_defined_acd_t {};
+struct nssai_inclusion_mode_t {};
+struct eap_message_t {};
+struct sor_transparent_container_t {};
+struct extended_emergency_number_list_t {};
+struct emergency_number_list_t {};
+struct t3502_t {};
+struct gprs_timer2_t {};
+struct n3g_deregistration_timer_t {};
+struct t3512_t {};
+struct service_area_list_t {};
+struct ladn_information_t {};
+struct guti_mobile_id_t {};
+struct requested_nssai_t {};
+struct mico_indication_t {};
+struct requested_drx_parametereter_t {};
+struct update_type_t {};
+struct payload_container_type_t {};
+struct aguti_mobile_id_t {};
+struct allowed_pdu_session_status_t {};
+struct pdu_session_status_t {};
+struct ue_usage_setting_t{};
+struct s1_ue_network_capability_t {};
+struct ue_security_capability_t {};
+struct ladn_indication_t {};
+struct payload_container_t {};
+struct nc_native_nas_ksi_t {};
+struct nas_ksi_t {};
+struct last_visited_tai_t {};
+struct tracking_area_id_t {};
+struct ue_status_t {};
+struct eps_nas_message_container_t {};
+struct nrmm_cause_t {};
+struct nrmm_capability_t {};
+struct network_feature_support_t {};
+struct network_slicing_indication_t {};
+struct abba_t {};
+} // namespace mm
